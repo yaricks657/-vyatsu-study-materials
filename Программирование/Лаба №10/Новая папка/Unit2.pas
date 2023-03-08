8b382cf4f982a203df299f875ac75c2e0e0241fa
yaricks657 <102881213+yaricks657@users.noreply.github.com>
2023-03-08 12:07:55 +0300

    Add files via upload

---
+++ Программирование/Лаба №10/Новая папка/Unit2.pas
@@ -0,0 +1,32 @@
+unit Unit2;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, StdCtrls;
+
+type
+  TForm2 = class(TForm)
+    lbl1: TLabel;
+    btn1: TButton;
+    procedure btn1Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form2: TForm2;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm2.btn1Click(Sender: TObject);
+begin
+Form2.Close;
+end;
+
+end.
