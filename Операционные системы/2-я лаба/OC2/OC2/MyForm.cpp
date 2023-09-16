5733e6c0c995a6504925113269c545880463b4f4
yaricks657 <102881213+yaricks657@users.noreply.github.com>
2023-09-16 17:40:19 +0300

    Да

--- Операционные системы/2-я лаба/OC2/OC2/MyForm.cpp
+++
@@ -1,9 +0,0 @@
-#include "MyForm.h"
-#include <Windows.h>
-using namespace OC2; // Название проекта
-int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
-	Application::EnableVisualStyles();
-	Application::SetCompatibleTextRenderingDefault(false);
-	Application::Run(gcnew MyForm);
-	return 0;
-}
